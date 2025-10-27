4575bbe3c49c527c3fe603f9930b9a5f4b369b0d
Alexander <alexande3@hollerith.cs.grinnell.edu>
2025-10-27 17:06:05 -0500

    Completed the sort event hierarchy!

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,31 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
  */
-public class CompareEvent<T> {
-    // TODO: implement me!
+public class CompareEvent<T> implements SortEvent<T> {
+    public int ind1;
+    public int ind2;
+
+    @Override
+    public void apply(T[] arr) {
+        // Intentionally left blank
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        List<Integer> lst = new ArrayList<>();
+        lst.add(ind1);
+        lst.add(ind2);
+        return lst;
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return false;
+    }
 }
--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -1,8 +1,37 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
  */
-public class SwapEvent<T> {
-    // TODO: implement me
+public class SwapEvent<T> implements SortEvent<T>{
+    public int ind1;
+    public int ind2;
+
+    SwapEvent(int ind1, int ind2) {
+        this.ind1 = ind1;
+        this.ind2 = ind2;
+    }
+
+    @Override
+    public void apply(T[] arr) {
+        T tmp = arr[ind1];
+        arr[ind1] = arr[ind2];
+        arr[ind2] = tmp;
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        List<Integer> lst = new ArrayList<>();
+        lst.add(ind1);
+        lst.add(ind2);
+        return lst;
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return true;
+    }
 }
